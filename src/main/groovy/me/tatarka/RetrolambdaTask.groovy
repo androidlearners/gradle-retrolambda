59076f0761a99299943c7a71d6d7382931649c8e
Evan Tatarka <evan@tatarka.me>
2014-03-04 22:41:29 -0500

    added license header

--- src/main/groovy/me/tatarka/RetrolambdaTask.groovy
+++ src/main/groovy/me/tatarka/RetrolambdaTask.groovy
@@ -1,4 +1,21 @@
+/**
+ Copyright 2014 Evan Tatarka
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package me.tatarka
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.JavaVersion
 import org.gradle.api.ProjectConfigurationException
@@ -11,6 +28,7 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.incremental.IncrementalTaskInputs
 
 import static me.tatarka.RetrolambdaPlugin.javaVersionToBytecode
+
 /**
  * Created by evan on 3/4/14.
  */
